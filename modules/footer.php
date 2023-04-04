da4eedb1ba03472ce3e51786175abade06d16593
anujachereddy <77001604+anujachereddy@users.noreply.github.com>
2023-04-04 09:31:31 +0530

    Add files via upload

---
+++ modules/footer.php
@@ -0,0 +1,49 @@
+</div>
+		<footer id="footer">
+			<div id="">
+			
+
+			
+				
+			</p>
+			</div>
+			<div id="links">
+			Developers: 
+				<a href="#" taget="_blank">Anuja Chereddy </a> 
+				<a href="#" taget="_blank"> Varshitha Done </a> 
+				<a href="#" taget="_blank"> Kashifa Shaik</a> 
+				<a href="#" taget="_blank"> Likhitha Atluri</a> 
+				<a href="#" taget="_blank"> Anusha Neerukonda</a>
+			</div>
+			
+		</footer>
+		<style>
+			footer {
+			position: fixed;
+  			bottom: 0;
+  			background-color: #373435;
+  			width: 100%;
+  			text-align: center; 
+  			color: #fff;
+  			padding-bottom: 10px;
+
+			}	
+			
+
+			#footer a{
+				margin-right:10px;
+				text-decoration: none;
+				color: white;
+				padding-left:15px;
+				padding-right:15px;
+				margin-bottom:10px;
+			}
+			footer a:hover{
+				color: white;
+			}
+
+		</style>
+		<script src="js/bootstrap.min.js"></script>
+    <script src="js/custom.js"></script>
+  </body>
+</html>
